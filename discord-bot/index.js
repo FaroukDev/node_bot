c84e1aa0dd47cc965dfb64fb3523b29b3754cd72
FaroukDev <fa.elayache1@gmail.com>
2021-03-16 14:10:26 +0100

    add token discord in .env

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,6 +1,10 @@
+console.log("Beep !!! Beep !!!");
+
+require('dotenv').config();
+
 const Discord = require("discord.js");
-const config = require("./config.json");
+// const config = require("./config.json");
 
 const client = new Discord.Client();
 
-client.login(config.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
