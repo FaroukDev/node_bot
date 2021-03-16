dbf5b01dc0a82cf8465ce4a9561b08c09080c9da
FaroukDev <fa.elayache1@gmail.com>
2021-03-16 10:58:25 +0100

    delete element on index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-const server = http.createServer(function (req, res){
-    res.writeHead(200);
-    res.end('Hello world')
-});
-server.listen(3000, () => console.log("listening..."));
