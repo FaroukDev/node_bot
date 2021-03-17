5ce145f320551b38b64094100e70c50680d81cf2
FaroukDev <fa.elayache1@gmail.com>
2021-03-17 13:50:47 +0100

    factorization of code into typescript

---
+++ discord-bot/index.js
@@ -0,0 +1,36 @@
+var Discord = require("discord.js");
+var config = require("./config.json");
+var client = new Discord.Client();
+client.on('ready', function () {
+    console.log("Logged in as " + client.user.tag + "!");
+});
+client.login(config.BOT_TOKEN);
+client.on("ready", readyDiscord);
+function readyDiscord() {
+    console.log("hello");
+}
+client.on("message", function (message) {
+    if (message.content === "!test") {
+        var channel01 = client.channels.cache.find(function (channel) { return channel.id === "821332759519690778"; });
+        channel01.send("hello les devclouds vous voulez faire du C dieze !");
+    }
+});
+client.on("message", function (message) {
+    if (message.content === "!hi") {
+        var channel01 = client.channels.cache.find(function (channel) { return channel.id === "821390897895047178"; });
+        channel01.send("hello les devclouds vous voulez faire du C dieze !");
+    }
+});
+client.on("ready", function () {
+    console.log("Bot is on");
+});
+client.on('message', function (message) {
+    var args = message.content.split(" ");
+    if (args[0].toLowerCase() === "nick") { //command is 'changeNick <@usertag> <new nickname>'
+        if (!message.mentions.users.first())
+            return message.channel.send("You didn't specify a user!");
+        var user = message.mentions.members.first();
+        var newNickname = args.slice(2).join(" ");
+        user.setNickname(newNickname);
+    }
+});
--- discord-bot/index.ts
+++ discord-bot/index.ts
@@ -1,10 +1,7 @@
-require('dotenv').config();
-
 const Discord = require("discord.js");
 const config = require("./config.json");
-const { Random } = require("something-random-on-discord")
 const client = new Discord.Client();
-const random = new Random();
+
 
 
 client.on('ready', () => {
@@ -17,26 +14,26 @@ function readyDiscord(){
     console.log("hello");
 }
 
-client.on("message", function(message){
+client.on("message", function(message: { content: string; }){
     if(message.content === "!test"){
-        const channel01 = client.channels.cache.find(channel => channel.id === "821332759519690778")
-    channel01.send("hello")
+        const channel01 = client.channels.cache.find((channel: { id: string; }) => channel.id === "821332759519690778")
+        channel01.send("hello les devclouds vous voulez faire du C dieze !")
     }  
 })
 
-client.on("message", function(message){
-    if(message.content === "!hi"){
-        const channel01 = client.channels.cache.find(channel => channel.id === "821390897895047178")
-    channel01.send("hello")
-    }  
-})
+client.on("message", (message: { content: string; }) => {
+        if (message.content === "!hi") {
+            const channel01 = client.channels.cache.find((channel: { id: string; }) => channel.id === "821390897895047178");
+            channel01.send("hello les devclouds vous voulez faire du C dieze !");
+        }
+    })
 
 client.on("ready", ()=> {
     console.log("Bot is on");
 })
 
 
-client.on('message', message => {
+client.on('message', (message: { content: string; mentions: { users: { first: () => any; }; members: { first: () => any; }; }; channel: { send: (arg0: string) => any; }; }) => {
 
     let args = message.content.split(" ");
 
