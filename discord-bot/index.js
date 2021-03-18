6c863ed0c072b828e8e20077e2f447341b4a3bf9
Adlane <50279061+aaadlane@users.noreply.github.com>
2021-03-18 11:20:36 +0100

    changement env

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,10 +1,10 @@
 var Discord = require("discord.js");
-var config = require("./config.json");
+// var config = require("./config.json");
 var client = new Discord.Client();
 client.on('ready', function () {
     console.log("Logged in as " + client.user.tag + "!");
 });
-client.login(config.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
 var readyDiscord = function () {
     console.log("hello");
 };
